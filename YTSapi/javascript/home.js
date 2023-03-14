199a8192b9c7ccd2371d11c28a5d32234ae1657c
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-03-14 21:42:33 +0545

    fetched list

--- YTSapi/javascript/home.js
+++ YTSapi/javascript/home.js
@@ -13,10 +13,10 @@ function getMovies() {
                 output += `
                 <div class="movie">
                     <h2 class="movie-title" title="${movieInfo.title}">${movieInfo.title_english}</h2>
-                    <img src="${movieInfo.large_cover_image}" alt="${movieInfo.title}">
+                    <img src="${movieInfo.medium_cover_image}" alt="${movieInfo.title}">
                     <input type="hidden" value="${movieInfo.id}">
-                    </div>
                     <button id="open" class="open" onclick="sendId(${movieInfo.id})">Details</button>
+                    </div>
                     <div class="movie">
                     </div>
             `;
@@ -31,7 +31,7 @@ function getMovies() {
 
 function sendId(movieId) {
     console.log(movieId);
-    // fetch(`https://yts.mx/api/v2/list_movies.json`)
+    // fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`)
     //     .then(response => {
     //         return response.json;
     //     }).then(movieDetails => {
